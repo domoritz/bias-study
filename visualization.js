d159df34e4a2aff57f858f5b261bfb750042d757
Daniel Epstein <depstein@cs.washington.edu>
2016-11-07 15:24:48 -0800

    error values within subject

--- visualization.js
+++ visualization.js
@@ -1,6 +1,7 @@
 //Generate all permutations of the visualization order, select by userID (which is effectively "random")
 //TODO: selected these states because they were popular for particular airlines (Delta, Southwest, Alaska). But it's worth thinking more about which states we want.
 var statesUsing = Combinatorics.permutation(['GA', 'NV', 'WA']).toArray();
+var amountErrorValues = ['1', '100', '200', '500', '1000']; //TODO: should we oversample the control condition?
 var visualizationsArray = statesUsing[userId%statesUsing.length];
 var airlinesByState = {'GA': ['Delta', 'ExpressJet', 'Southwest', 'American', 'United'], 'NV': ['Southwest', 'United', 'Spirit', 'Delta', 'American'], 'WA': ['Alaska', 'United', 'Delta', 'Southwest', 'SkyWest']};
 
@@ -8,8 +9,24 @@ var nextPage = {'approximate':'precise.html', 'precise':'questions.html'};
 
 function updateData(label) {
 	var currentSeen = parseInt(localStorage.getItem(label + "Seen"));
+	var statLookingAt = visualizationsArray[currentSeen];
 	//Update the database with how many visualizations the client claims to have seen.
 	newLog.child(label + "Seen").set(currentSeen);
+
+	var amountError = amountErrorValues[Math.floor(Math.random() * amountErrorValues.length)];
+	var sequenceNumber = amountError == '1' ? '0' : Math.ceil(Math.random()*20).toString();
+	var howMany = airlinesByState[statLookingAt][Math.floor(Math.random()*airlinesByState[statLookingAt].length)];
+	var percentage = airlinesByState[statLookingAt][Math.floor(Math.random()*airlinesByState[statLookingAt].length)];
+
+	//Store error, sequence number, question parameters in database.
+	var visRecord = newLog.child(label + 'Vis' + (currentSeen+1)); //1-index for our own sanity
+	visRecord.child('amountError').set(amountError);
+	visRecord.child('sequenceNumber').set(sequenceNumber);
+	visRecord.child('howMany').set(howMany);
+	visRecord.child('percentage').set(percentage);
+
+	console.log("Error amount: " + amountError);
+	console.log("Sequence number: " + sequenceNumber);
 
 	if(label == 'approximate') { //Always use sequence number 1
 		$('#visualization').prepend("<img src='data/images/" + visualizationsArray[currentSeen] + "_" + amountError + "_" + sequenceNumber + ".png' width='600px'>");
@@ -17,30 +34,25 @@ function updateData(label) {
 		$('#visualization').prepend("<img src='data/images/" + visualizationsArray[currentSeen] + "_1_0.png' width='600px'>");
 	}
 
-	var howMany = airlinesByState[visualizationsArray[currentSeen]][Math.floor(Math.random()*airlinesByState[visualizationsArray[currentSeen]].length)];
-	var percentage = airlinesByState[visualizationsArray[currentSeen]][Math.floor(Math.random()*airlinesByState[visualizationsArray[currentSeen]].length)];
 	$('#howMany').text(howMany);
 	$('#percentage').text(percentage);
-	//TODO: store these values in the DB. Hopefully they don't influence results...
-
 	$('#visNum').text(currentSeen + 1); //1-indexted for showing to humans
 
-	if(currentSeen >= visualizationsArray.length - 1) { //go to the next page after this visualization
-	    $('#form').attr("action", nextPage[label]);
-	  }
-
-  $('#form').submit(function(ev) {
+	$('#form').submit(function(ev) {
 		ev.preventDefault();
 		currentSeen++; //increment the number we've seen
 		/*TODO: some sort of sensible bounds checking?
 		If someone revisits approximate.html after finishing the approximate vis's, for example, it'll try to find a 4th one.*/
 		localStorage.setItem(label + "Seen", currentSeen);
 		newLog.child(label + "Seen").set(currentSeen);
-		var visRecord = newLog.child(label + 'Vis' + currentSeen);
 		$.each($('#form').serializeArray(), function(i, field) {
-    		visRecord.child(field.name).set(field.value);
+			visRecord.child(field.name).set(field.value);
 		});
 		visRecord.child('whichVisualization').set(visualizationsArray[currentSeen-1]);
-		this.submit();
+		if(currentSeen >= visualizationsArray.length) { //go to the next page after this visualization
+			window.location.href = nextPage[label];
+		} else {
+			window.location.href = label + '.html';
+		}
 	});
 }
