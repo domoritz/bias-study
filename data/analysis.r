9c9479d04f5a56f69d0476d5420ae8c866d76609
Daniel Epstein <depstein@cs.washington.edu>
2016-12-07 09:30:40 -0800

    plots for cleaned data

--- data/analysis.r
+++ data/analysis.r
@@ -3,7 +3,7 @@ library(ggplot2)
 library(mefa)
 library(lsmeans)
 
-bias_data <- read.csv("second-batch.csv")
+bias_data <- read.csv("cleaned_data.csv")
 #Select the columns for statistics
 id <- rep(as.vector(t(bias_data["id"])), each=2)#airline, states
 condition <- rep(as.vector(t(bias_data["studyCondition"])), each=2)#airline, states
@@ -16,10 +16,10 @@ cleaned_data_frame <- data.frame(id, condition, visType, focus, sequence, order)
 #-----How many questions when viewing the precise data-----
 how_many_precise <- cleaned_data_frame
 #Calcualte approximate error
-approx_howMany_answer <- as.vector(t(bias_data[c("approx.airline.howMany.answer", "approx.states.howMany.answer")]))
-approx_howMany_approx <- as.vector(t(bias_data[c("approx.airline.howMany.approx", "approx.states.howMany.approx")]))
-precise_howMany_precise <- as.vector(t(bias_data[c("precise.airline.howMany.precise", "precise.states.howMany.precise")]))
-precise_howMany_answer <- as.vector(t(bias_data[c("precise.airline.howMany.answer", "precise.states.howMany.answer")]))
+approx_howMany_answer <- as.vector(t(bias_data[c("approx_airline_howMany_answer", "approx_states_howMany_answer")]))
+approx_howMany_approx <- as.vector(t(bias_data[c("approx_airline_howMany_approx", "approx_states_howMany_approx")]))
+precise_howMany_precise <- as.vector(t(bias_data[c("precise_airline_howMany_precise", "precise_states_howMany_precise")]))
+precise_howMany_answer <- as.vector(t(bias_data[c("precise_airline_howMany_answer", "precise_states_howMany_answer")]))
 
 how_many_precise$approximate_error <- (approx_howMany_answer - approx_howMany_approx)/ approx_howMany_approx
 how_many_precise$expected_bias <- (precise_howMany_precise - approx_howMany_approx)/ precise_howMany_precise
@@ -37,13 +37,13 @@ ggsave("plots/how_many_precise.png", how_many_precise_plot)
 compare_precise <- rep(cleaned_data_frame, 2)
 #Calculate approximate error (Only looking at the first approximate answer, since I think this was the intended behavior)
 
-approx_compare_answer <- as.vector(t(bias_data[c("approx.airline.howManyCompare.0.answer", "approx.states.howManyCompare.0.answer")]))
-approx_compare_approx <- as.vector(t(bias_data[c("approx.airline.howManyCompare.0.approx", "approx.states.howManyCompare.0.approx")]))
-precise_compare_approx <- as.vector(t(bias_data[c("precise.airline.howManyCompare.0.approx", "precise.states.howManyCompare.0.approx", "precise.airline.howManyCompare.1.approx", "precise.states.howManyCompare.1.approx")]))
-precise_compare_precise <- as.vector(t(bias_data[c("precise.airline.howManyCompare.0.precise", "precise.states.howManyCompare.0.precise", "precise.airline.howManyCompare.1.precise", "precise.states.howManyCompare.1.precise")]))
-precise_compare_answer <- as.vector(t(bias_data[c("precise.airline.howManyCompare.0.answer", "precise.states.howManyCompare.0.answer", "precise.airline.howManyCompare.1.answer", "precise.states.howManyCompare.1.answer")]))
-precise_compare_comparison <- as.vector(t(bias_data[c("precise.airline.howManyCompare.0.data", "precise.states.howManyCompare.0.data", "precise.airline.howManyCompare.1.data", "precise.states.howManyCompare.1.data")]))
-approx_compare_comparison <- as.vector(t(bias_data[c("approx.airline.howManyCompare.0.data", "approx.states.howManyCompare.0.data")]))
+approx_compare_answer <- as.vector(t(bias_data[c("approx_airline_howManyCompare_0_answer", "approx_states_howManyCompare_0_answer")]))
+approx_compare_approx <- as.vector(t(bias_data[c("approx_airline_howManyCompare_0_approx", "approx_states_howManyCompare_0_approx")]))
+precise_compare_approx <- as.vector(t(bias_data[c("precise_airline_howManyCompare_0_approx", "precise_states_howManyCompare_0_approx", "precise_airline_howManyCompare_1_approx", "precise_states_howManyCompare_1_approx")]))
+precise_compare_precise <- as.vector(t(bias_data[c("precise_airline_howManyCompare_0_precise", "precise_states_howManyCompare_0_precise", "precise_airline_howManyCompare_1_precise", "precise_states_howManyCompare_1_precise")]))
+precise_compare_answer <- as.vector(t(bias_data[c("precise_airline_howManyCompare_0_answer", "precise_states_howManyCompare_0_answer", "precise_airline_howManyCompare_1_answer", "precise_states_howManyCompare_1_answer")]))
+precise_compare_comparison <- as.vector(t(bias_data[c("precise_airline_howManyCompare_0_data", "precise_states_howManyCompare_0_data", "precise_airline_howManyCompare_1_data", "precise_states_howManyCompare_1_data")]))
+approx_compare_comparison <- as.vector(t(bias_data[c("approx_airline_howManyCompare_0_data", "approx_states_howManyCompare_0_data")]))
 
 compare_precise$approximate_error <- rep((approx_compare_answer - approx_compare_approx)/approx_compare_approx, 2)
 compare_precise$expected_bias <- (precise_compare_precise - precise_compare_approx)/precise_compare_precise
@@ -61,9 +61,9 @@ ggsave("plots/compare_precise.png", compare_precise_plot)
 #-----Question of "how many of X were there?-----
 jaccard_precise <- cleaned_data_frame
 
-jaccard_precise$expected_bias <- as.vector(t(bias_data[c("precise.states.SelectAll.jaccard_answer_precise", "precise.airline.SelectAll.jaccard_answer_precise")]))
-jaccard_precise$approximate_error <- as.vector(t(bias_data[c("precise.states.SelectAll.jaccard_approx_precise", "precise.airline.SelectAll.jaccard_approx_precise")]))
-jaccard_precise$measured_bias <- as.vector(t(bias_data[c("precise.states.SelectAll.jaccard_approx_answer", "precise.airline.SelectAll.jaccard_approx_answer")]))
+jaccard_precise$expected_bias <- as.vector(t(bias_data[c("precise_states_SelectAll_jaccard_answer_precise", "precise_airline_SelectAll_jaccard_answer_precise")]))
+jaccard_precise$approximate_error <- as.vector(t(bias_data[c("precise_states_SelectAll_jaccard_approx_precise", "precise_airline_SelectAll_jaccard_approx_precise")]))
+jaccard_precise$measured_bias <- as.vector(t(bias_data[c("precise_states_SelectAll_jaccard_approx_answer", "precise_airline_SelectAll_jaccard_approx_answer")]))
 
 jaccard_precise_test <- lm(expected_bias ~ approximate_error + measured_bias + condition + (1 | id), data=jaccard_precise)
 
